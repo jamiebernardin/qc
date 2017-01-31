4379ba1e33f27e7d6face1c7b252e3f409555125
jamiebernardin <jamie.bernardin@gmail.com>
2017-01-31 06:33:43 -0500

    fixed index

--- ten_prod.py
+++ ten_prod.py
@@ -6,13 +6,13 @@ import numpy as np
 def ten_prod(a, b):
     nj, nk = a.shape
     nl, nm = b.shape
-    rows = nj + nl
-    columns = nk + nm
+    rows = nj * nl
+    columns = nk * nm
     tp = np.zeros((rows, columns))
-    for j in range(0, nj):
+    for i in range(0, nj):
         for k in range(0, nk):
             for l in range(0, nl):
                 for m in range(0, nm):
-                    tp[(j-1)*nl+l, (k-1)*nk + m] = a[j, k]*b[l, m]
+                    tp[i*nl+l, k*nk + m] = a[i, k]*b[l, m]
     return tp
 
